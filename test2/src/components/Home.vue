127b8512a7a715a792ba5c6087ef4f0b2f125acc
comseung18 <comseung18@koreatech.ac.kr>
2020-11-11 23:43:10 +0900

    yarn add vue-router

---
+++ test2/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- About.vue 도 Home.vue와 동일하게 작성합니다. -->
+  <div id="example">
+    <div class="panel panel-default">
+      <div class="panel-heading">Home</div>
+      <div class="panel-body">Home 화면입니다</div>
+    </div>
+  </div>
+</template>
+
+<style>
+#example{
+  margin: 10px auto;
+  max-width: 820px;
+  min-width: 820px;
+  padding: 0px;
+  position: relative;
+  font: 13px "verdana";
+}
+</style>
